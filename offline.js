bc27c262af7db98e402b21aca76fb2e37488c0cf
alg-delta <155776375+alg-delta@users.noreply.github.com>
2024-06-04 01:44:37 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1717454598,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/trava-sheet0.png",
+		"images/pyxca-sheet0.png",
+		"images/bauactar-sheet0.png",
+		"images/coleso-sheet0.png",
+		"images/stena-sheet0.png",
+		"images/pizdutin-sheet0.png",
+		"images/locavia-sheet0.png",
+		"images/palca-sheet0.png",
+		"images/xtyca-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
